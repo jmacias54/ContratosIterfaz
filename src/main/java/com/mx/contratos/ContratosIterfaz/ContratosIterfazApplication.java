e4ae2538799722d4094d2d9e7add77b30826649b
jesus armando macias benitez <jesusmb@finerioconnect.com>
2024-01-09 10:57:52 -0600

    First commit

---
+++ src/main/java/com/mx/contratos/ContratosIterfaz/ContratosIterfazApplication.java
@@ -0,0 +1,13 @@
+package com.mx.contratos.ContratosIterfaz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContratosIterfazApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContratosIterfazApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mx/contratos/ContratosIterfaz/ContratosIterfazApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mx.contratos.ContratosIterfaz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContratosIterfazApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
